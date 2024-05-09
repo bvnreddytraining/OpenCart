84b89b01aef3795f225642bc50c7fd3caf1009b2
bvnreddytraining <bvnreddytutorial@gmail.com>
2024-05-09 12:11:11 +0530

    Update TC_007_GitTEST.java
    Testing Pull Command

--- src/test/java/testCases/TC_007_GitTEST.java
+++ src/test/java/testCases/TC_007_GitTEST.java
@@ -3,5 +3,6 @@ package testCases;
 public class TC_007_GitTEST {
 		public void testMethod() {
 			System.out.println("This is my Git Test");
+			System.out.println("PULL - This is my Git Test");
 		}
 }
